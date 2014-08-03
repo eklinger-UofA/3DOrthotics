26d54fb6da53606d5a1cba14427cdf343331ae22
eklinger-UofA <eklinger@ualberta.ca>
2014-08-02 20:27:18 -0600

    Fixed up pep8 issues with initial files

--- orthotics_project/clients/urls.py
+++ orthotics_project/clients/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 from clients import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'))
+urlpatterns = \
+    patterns('',
+             url(r'^$', views.index, name='index')
+             )
--- orthotics_project/orthotics_project/urls.py
+++ orthotics_project/orthotics_project/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'orthotics_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = \
+    patterns('',
+             # Examples:
+             # url(r'^$', 'orthotics_project.views.home', name='home'),
+             # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^clients/', include('clients.urls')),
-)
+             url(r'^admin/', include(admin.site.urls)),
+             url(r'^clients/', include('clients.urls')),
+             )
