2336582beb57263e25b7b856d2609d616aa77380
eklinger-UofA <eklinger@ualberta.ca>
2014-08-09 11:41:27 -0600

    Updated client models file
    Added most of the functionality for a client model.
    Added client model to the admin page
    Tested fields to admin panel, settings required and not.
    `

--- orthotics_project/clients/admin.py
+++ orthotics_project/clients/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 # import models once i am done writing them
 # from <app>.models import <model_name>, <model_name>
+from clients.models import Client
 
 # Register your models here.
 
 # admin.site.register(<model_name>)
+admin.site.register(Client)
--- orthotics_project/clients/models.py
+++ orthotics_project/clients/models.py
@@ -1,4 +1,4 @@
-"""Models for the clients app
+"""Models for the clients app.
 
 The following tables will be contained within:
 - Client
@@ -7,69 +7,96 @@ The following tables will be contained within:
 - Claims
 
 """
-
 from django.db import models
 
-"""
-Table: Client
-Fields:
-First Name
-Last Name
-Address
-Phone #
-Cell #
-Email
-Birthdate
-Credit (current credit from insurance company)
-Notes (for log of communication)
-Dependants - another table
-Prescriptions - another table
-Insurance - another table
-"""
+
+class Client(models.Model):
+    """Model of a client in our system
+
+    A client will have the following fields:
+    First Name
+    Last Name
+    Address
+    Phone #
+    Cell #
+    Email
+    Birthdate
+    Credit (current credit from insurance company)
+    Notes (for log of communication)
+    Dependants - another table
+    Prescriptions - another table
+    Insurance - another table
+    """
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = ((MALE, 'Male'),
+                      (FEMALE, 'Female'))
+
+    firstName = models.CharField(max_length=128)
+    lastName = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    # TODO Write validators for the phonenumbers below
+    phoneNumber = models.CharField(max_length=14, blank=True, null=True) # In the form of (780)-937-1514
+    cellNumber = models.CharField(max_length=14, blank=True, null=True) # In the form of (780)-937-1514
+    email = models.EmailField(max_length=254, blank=True, null=True) # will cover all RFC3696/5321-compliant email addresses
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    credit = models.SmallIntegerField(default=0)
+# insurance (foreign key)
+# dependants (foreign key)
+# Perscriptions (foreign key)
+# invoices (foreign key)
+# claims (foreign key)
+
+    def __unicode__(self):
+        return self.firstName + " - " + self.lastName
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def getAge(self):
+        """Calculate clients age in years."""
+        pass
+
+    """
+    Table Prescriptions:
+    Fields:
+    Client
+    Date scanned
+    Prescription image
+    """
+
+    """
+    Table: Insurance
+    Fields:
+    Husband and Wifes coverage (there could be multiple policies)
+    Provider
+    Policy #
+    Contract #
+    coverage %
+    Benefit year (when the insurers benefit period renews):
+    1) Calendar Year (Jan 1)
+    2) could be a specific month in the year
+    Example: April 1
+
+    Direct/indirect billing
+    coverage conditions
+    """
 
 
-"""
-Table Prescriptions:
-Fields:
-Client
-Date scanned
-Prescription image
-"""
-
-"""
-Table: Insurance
-Fields:
-Husband and Wife’s coverage (there could be multiple policies)
-Provider
-Policy #
-Contract #
-coverage %
-Benefit year (when the insurers benefit period renews):
-1) Calendar Year (Jan 1)
-2) could be a specific month in the year
-Example: April 1
-
-Direct/indirect billing
-coverage conditions
-"""
+    """
+    Table: Claims
+    Fields:
+    Submitted date
+    invoice date
+    paid date
+    client/dependant
+    insurance
+    amount claimed
+    expected back
+    payment type (cash, cheque, credit)
+    Look up report types from google docs
+    """
 
 
-"""
-Table: Claims
-Fields:
-Submitted date
-invoice date
-paid date
-client/dependant
-insurance
-amount claimed
-expected back
-payment type (cash, cheque, credit)
-Reports:
-1) Outstanding Payments
-- 15/30/60/90 day cycle (insurance company “Direct billing”/patient “Non-direct billing”)
-- Paid claims 15/30/60/90 days (or possibly if we can have a have a calendar where we can pick from a specific start date to and end date)
-2) Paid claims 15/30/60/90 days (or possibly if we can have a have a calendar where we can pick from a specific start date to and end date)
-3) Monthly breakdown of revenue
-    - amount invoiced, amount expected, amount actually paid (possibly a graph, or a table)
-"""
+
