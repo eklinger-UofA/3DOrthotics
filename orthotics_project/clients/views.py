4cba0094fc04fc3d4c77edac3e3d769402dc3dd0
Appolloyon <cklinger@ualberta.ca>
2014-08-09 21:44:13 -0600

    Enhanced client index page.

--- orthotics_project/clients/views.py
+++ orthotics_project/clients/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-
+from clients.models import Client
 
 def index(request):
     context = RequestContext(request)
 
-    context_dict = {'bold_message': 'This is a message to be bolded'}
+    client_list = Client.objects.all()
+    client_dict = {'clients': client_list}
 
-    return render_to_response('clients/template1.html', context_dict, context)
+    return render_to_response('clients/template1.html', client_dict, context)
