0473b706f2c63e8805cfd5cbc8978a65e48c8a01
eklinger-UofA <eklinger@ualberta.ca>
2014-08-16 14:02:42 -0600

    Added basic inventory page

--- orthotics_project/orthotics_project/urls.py
+++ orthotics_project/orthotics_project/urls.py
@@ -10,7 +10,8 @@ urlpatterns = \
              # url(r'^$', 'orthotics_project.views.home', name='home'),
              # url(r'^blog/', include('blog.urls')),
 
+             url(r'^$', 'orthotics_project.views.index', name='index'),
              url(r'^admin/', include(admin.site.urls)),
              url(r'^clients/', include('clients.urls')),
-             url(r'^$', 'orthotics_project.views.index', name='index')
+             url(r'^inventory/', include('inventory.urls')),
              )
