6fc798b951360c77ac55e14f27b9876ec5ccb376
eklinger-UofA <eklinger@ualberta.ca>
2014-08-23 11:58:47 -0600

    Started work on dependants tab

--- orthotics_project/clients/views.py
+++ orthotics_project/clients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
-from clients.models import Client
+from clients.models import Client, Dependent
 
 def index(request):
     context = RequestContext(request)
@@ -17,7 +17,18 @@ def clientView(request, client_id):
 
     client = Client.objects.get(id=client_id)
     insurance = client.insurance_set.all()
+    dependents = client.dependents.all()
+    spouse = None
+    children = []
+    for dependent in dependents:
+        if dependent.relationship == Dependent.SPOUSE:
+            spouse = dependent
+        else:
+            children.append(dependent)
 
     context_dict = {'client': client,
-                    'client_insurance': insurance}
+                    'client_insurance': insurance,
+                    'spouse': spouse,
+                    'children': children}
+    print context_dict
     return render_to_response('clients/client.html', context_dict, context)
